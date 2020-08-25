26459095bfc3658dc36a950969ead068f3496077
unknown <bakti.satria.a@gmail.com>
2020-08-25 13:56:58 +0700

    update db and seeder

--- app/Database/Migrations/2020-08-24-065813_lahan_migration.php
+++ app/Database/Migrations/2020-08-24-065813_lahan_migration.php
@@ -15,64 +15,82 @@ class LahanMigration extends Migration
 			],
 			'idareal'			=> [
 				'type'			=> 'INT',
+				'null'			=> TRUE,
 				'constraint'	=> 11
 			],
 			'kode_lokasi'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 11
 			],
 			'area_regional'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 11
 			],
 			'kode_provinsi'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 11
 			],
 			'kode_witel'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 11
 			],
 			'nama_lokasi'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'luas'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'alamat_lokasi'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'provinsi'			=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'kota'				=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'kecamatan'			=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'desa'				=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'latitude'			=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'longitude'			=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			],
 			'peruntukan'		=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
-			]
+			],
+			'created_at datetime default current_timestamp',
+    		'updated_at datetime default current_timestamp on update current_timestamp'
+			
 
 
 		]);
--- app/Database/Migrations/2020-08-24-121318_witel_migration.php
+++ app/Database/Migrations/2020-08-24-121318_witel_migration.php
@@ -16,13 +16,17 @@ class WitelMigration extends Migration
 			],
 			'witel'			=> [
 				'type'			=> 'VARCHAR',
+				'null'			=> TRUE,
 				'constraint'	=> 255
 			
 			],
 			'kode_witel'		=> [
 				'type'			=> 'INT',
+				'null'			=> TRUE,
 				'constraint'	=> 11
-			]
+			],
+			'created_at datetime default current_timestamp',
+    		'updated_at datetime default current_timestamp on update current_timestamp'
 		]);
 
 		$this->forge->addKey('id', TRUE);
