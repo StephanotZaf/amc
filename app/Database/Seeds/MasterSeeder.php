bc32c2a4ff4bbf8ae83b8bf12e2d11002540996a
unknown <bakti.satria.a@gmail.com>
2020-09-01 09:45:31 +0700

    update seeder

--- app/Database/Seeds/MasterSeeder.php
+++ app/Database/Seeds/MasterSeeder.php
@@ -4,7 +4,18 @@ class MasterSeeder extends \CodeIgniter\Database\Seeder
 {
         public function run()
         {
+                $db = \Config\Database::connect();
+                
+                $builder = $db->table('treg');
+                $builder->emptyTable('treg');
                 $this->call('TregSeeder');
+
+                $builder = $db->table('witel');
+                $builder->emptyTable('witel');
                 $this->call('WitelSeeder');
+
+                $builder = $db->table('provinsi');
+                $builder->emptyTable('provinsi');
+                $this->call('ProvinsiSeeder');
         }
 }
